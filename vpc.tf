142bf29a294bfe823df3e27ff19eb1fd27ec9978
Cameron Li <cameronl4320@gmail.com>
2023-03-01 22:32:22 +0000

    Renaing network

--- vpc.tf
+++ vpc.tf
@@ -2,9 +2,9 @@ module "network" {
   source  = "app.terraform.io/trademe-cat/network/google"
   version = "3.4.0"
   # insert required variables here
-  network_name = "hashi-cat"
+  network_name = "other-cat"
   project_id = var.project
-  
+
   subnets = [
   {
     subnet_name   = "gaurav-subnet"
