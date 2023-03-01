80731ff2b2389642f059b10a53f36367ae963854
Cameron Li <cameronl4320@gmail.com>
2023-03-01 22:08:01 +0000

    Attempt 2

--- vpc.tf
+++ vpc.tf
@@ -2,16 +2,16 @@ module "vpc" {
     source  = "terraform-google-modules/network/google"
     version = "~> 3.0"
 
-    project_id   = "var.project"
+    project_id   = var.project_id
     network_name = "hashi-cat"
     routing_mode = "GLOBAL"
 
     subnets = [
-    {
-        subnet_name   = "gaurav-subnet"
-        subnet_ip     = "10.100.10.0/24"
-        subnet_region = var.region
-    }
+      {
+          subnet_name   = "gaurav-subnet"
+          subnet_ip     = "10.100.10.0/24"
+          subnet_region = var.region
+      }
     ]
 
     secondary_ranges = {
